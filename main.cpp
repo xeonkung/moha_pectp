8a31d0af2c8ad268ee0b2919f6d29099e01779ff
xeonkung <xeonkung@abc.local>
2013-11-04 15:23:52 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
-using namespace std
-int main() {
-  cout << "Hello!" << endl
-  return 0
+#include <string>
+#include <fstream>
+using namespace std;
+
+/* PECTP FILE Class */
+class PECTPFile {
+  private:
+  	ifstream _file;
+  public:
+  	void read (const char*);
+};
+void PECTPFile::read (const char* filename) {
+	string line;
+	_file.open(filename);
+	if (_file.is_open()) {
+
+	}else{
+		// log error
+	}
+}
+
+
+int main (int argc, char* argv[]) {
+	cout << "Hello!" << endl;
+	for(int i = 0; i < argc; i++)
+		cout << "argv[" << i << "] = " << argv[i] << endl; 
+	return 0;
 }
